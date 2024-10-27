236dd217cf2319464496f6ed22492914c0b7d241
JohnSoi <un.perso@yandex.ru>
2024-10-27 17:05:57 +0500

    Добавляем отправку в HAWK при стандартных ошибках

--- dh_base/exceptions.py
+++ dh_base/exceptions.py
@@ -14,5 +14,5 @@ class BaseAppException(HTTPException):
     DETAIL: str | None
 
     def __init__(self):
-        hawk.send(HTTPException, {'code': self.STATUS_CODE, 'detail': self.DETAIL})
+        hawk.send(RuntimeError, {'code': self.STATUS_CODE, 'detail': self.DETAIL, 'http+exception': True})
         super().__init__(status_code=self.STATUS_CODE, detail=self.DETAIL)
