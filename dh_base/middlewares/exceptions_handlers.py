8294044eaae068fa25e62e9a7144cc23d4c97f23
JohnSoi <un.perso@yandex.ru>
2024-10-27 16:56:58 +0500

    Правка middleware

--- dh_base/middlewares/exceptions_handlers.py
+++ dh_base/middlewares/exceptions_handlers.py
@@ -6,8 +6,7 @@ from ..logger import logger, hawk
 async def exceptions_handler(request: Request, call_next):
     """Обрабатывает исключения с отправкой данных в HAWK"""
     try:
-        response = await call_next(request)
-        return response
+        return await call_next(request)
     except Exception as ex:
         hawk.send()
         logger.error("Ошибка исполнения", extra={"exc": ex})
